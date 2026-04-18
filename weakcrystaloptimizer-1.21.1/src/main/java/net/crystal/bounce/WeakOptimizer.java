cade30a90f6f21afdfbf90c92e976d38cfbcb455
arasscarpe <235102537+arasscarpe@users.noreply.github.com>
2026-04-18 16:45:29 +0300

    Delete weakcrystaloptimizer-1.21.1 directory

--- weakcrystaloptimizer-1.21.1/src/main/java/net/crystal/bounce/WeakOptimizer.java
+++
@@ -1,9 +0,0 @@
-package net.crystal.bounce;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class WeakOptimizer implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-    }
-}
