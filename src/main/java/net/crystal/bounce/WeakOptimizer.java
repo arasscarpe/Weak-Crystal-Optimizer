e744868dbe253957f8e5d7c4d17daf17e8aae5cc
arasscarpe <235102537+arasscarpe@users.noreply.github.com>
2026-04-18 16:46:09 +0300

    Add files via upload

---
+++ src/main/java/net/crystal/bounce/WeakOptimizer.java
@@ -0,0 +1,9 @@
+package net.crystal.bounce;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeakOptimizer implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
